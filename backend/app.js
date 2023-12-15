823ff214ab9a3a51e5d5be56c9ef0f8d843ebb5d
ginx836 <141227582+ginx836@users.noreply.github.com>
2023-12-15 15:57:50 +0100

    1ère route fonctionnelle pour tester la communication FE/BE
    Création d'une 1ère route pour tester la réponse du FrontEnd au serveur

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-  res.json({ message: 'Your request was successful!' });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+  next();
+});
+
+app.use('/api/books', (req, res) => {
+  const book = [
+    {
+      userId: '1',
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: 'J.K. Rowling',
+      imageUrl: 'https://m.media-amazon.com/images/I/81gOJoEgVoL._SL1500_.jpg',
+      year: 1998,
+      genre: 'Fantasy'
+    }
+  ];
+
+res.status(200).json(book);
+
 });
 
 module.exports = app;
