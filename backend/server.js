aeedbed0e1ff3e49bde5624b16086518c1f615d8
ginx836 <141227582+ginx836@users.noreply.github.com>
2023-12-15 15:42:01 +0100

    Initialisation Express
    Installation d'EXPRESS
    Création du fichier app.js
    Modification du fichier server.js pour intégrer l'application Express

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const dotenv = require('dotenv');
 const result = dotenv.config();
 
+const app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.end('Voila la reponse du serveur !');
-});
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
 
